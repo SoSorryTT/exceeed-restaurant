a66b6da743715e323b7ae974bebc8cd01f425b7d
Paradorn-248 <paradorn248@gmail.com>
2022-02-06 15:21:24 +0700

    get_del

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from matplotlib.pyplot import table
 from pymongo import MongoClient
 from pydantic import BaseModel
 
@@ -10,10 +11,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["exceed_restaurant"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
 
 app = FastAPI()
 
@@ -21,11 +22,21 @@ app = FastAPI()
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    query = {"name":name}
+    r = collection.find(query,{"_id":0,"name":1,"time":1,"table_number":1})
+    listt = list()
+    for i in r:
+        listt.append(i)
+    return {"result":listt}
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    query = {"table_number":table}
+    r = collection.find(query,{"_id":0,"name":1,"time":1,"table_number":1})
+    listt = list()
+    for i in r:
+        listt.append(i)
+    return {"result":listt}
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
@@ -37,5 +48,5 @@ def update_reservation(reservation: Reservation):
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    pass
-
+    query = {'name':name,'table_number':table}
+    collection.delete_one(query)
